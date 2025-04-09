b1c632cf065a8fd23b275cc8ce9613916f6d7a5f
Manuel Domingos <jonathamarielson@gmail.com>
2025-04-09 12:13:51 +0100

    primeira mensagem

---
+++ main.ts
@@ -0,0 +1,7 @@
+function multiply(a: number, b: number): number {
+  return a * b;
+}
+
+console.log(multiply(2, 20)); // Output: 6
+
+export {multiply}; // This line is added to ensure the file is treated as a module
